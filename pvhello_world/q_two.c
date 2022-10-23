74f101c6e0c9e4fc7fb542d526baa3039c9454f7
EmekaG <nellychukwemeka@gmail.com>
2022-10-23 03:03:09 -0700

    q_two.c

---
+++ pvhello_world/q_two.c
@@ -0,0 +1,4 @@
+/* here the -c command is used for compilation to bring out an object code
+ * used by the linker, but because it won't link to a new file destination,
+ * The -o command is not needed for the linker */
+gcc -c filename
