df71c2ae17492043c5f4248eded2abe4bc1b4ace
EmekaG <nellychukwemeka@gmail.com>
2022-10-23 02:31:30 -0700

    q_one.c

---
+++ pvhello_world/README.md
@@ -0,0 +1,6 @@
+This is a README.md file containing all the contents for hello World project
+
+* Question 1
+* Write a script that runs a C file through the preprocessor and save the result into another file.
+* The C file name will be saved in the variable $CFILE
+* The output should be saved in the file c
---
+++ pvhello_world/q_one.c
@@ -0,0 +1,6 @@
+/* The -E signifies the file going through the preprocessor*/
+/* The file, called 'filename' after going through the preprocessor
+ *  is saved into a newfile called 'newfilename' with the help of -o command */
+/* gcc command is the major command for compilation */
+gcc -E filename -o newfilename
+
