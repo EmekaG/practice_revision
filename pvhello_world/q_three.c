dc0ef3fee2fc17dc9abc225d1d675c36491439bc
EmekaG <nellychukwemeka@gmail.com>
2022-10-23 04:05:31 -0700

    q_three.c

---
+++ pvhello_world/q_three.c
@@ -0,0 +1,3 @@
+/* The -S command is used to produce an assembly code for a file going
+ * through compilation process */
+gcc -S filename
--- pvhello_world/q_two.c
+++ pvhello_world/q_two.c
@@ -1,4 +1,5 @@
 /* here the -c command is used for compilation to bring out an object code
- * used by the linker, but because it won't link to a new file destination,
- * The -o command is not needed for the linker */
+ * used by the linker, this process goes through assemblier to bring out the
+ * object code, but because it won't link to a new file destination,
+ * The -o command is not needed */
 gcc -c filename
